9f53b875fa56a73609c2c42f4e077d1b4432adee
StrawberryMikey <131277665+sofiaProbstFerreira@users.noreply.github.com>
2023-10-27 12:29:13 -0300

    Add files via upload

---
+++ conversãoDetipos.js
@@ -0,0 +1,6 @@
+console.log ("2"+"2");
+console.log(parseInt("2") + parseInt("2"));
+console.log ("10"/"2"); //mostrar102
+console.log("nome"/"2"); //not a number
+console.log (6.5); //imprime 6.5
+console.log (6,5); //imprime 6,5
---
+++ trabalhandocomatribuiçõesVariaveis.js
@@ -0,0 +1,7 @@
+console.log ("trabalhandocomatribuições")
+const idade= "17";
+const nome= "Mikey";
+const sobrenome= "Probst";
+console.log (nome,sobrenome); // Mikey Probst
+console.log (nome+" "+sobrenome); //Mikey Probst
+console.log (`meu nome é ${nome} ${sobrenome},${nome} ${sobrenome}`); //interpolação de variaveis
